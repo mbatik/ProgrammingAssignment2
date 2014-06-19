ecb503e6e94eaf38e1a78a5dcd191ac9be80580d
mbatik <mbatik@gmail.com>
2014-06-19 14:15:43 -0500

    Added assignment code
    added code that would allow for the cache of a matrix and the ability
    to take that matrix and return its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow for the cache of a matrix and then allows
+## the return of the inverse of that matrix
 
-## Write a short comment describing this function
+## makeCacheMatix caches a matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+}
+        get <- function() x
+        setmatrix <- function(x) m <<- x
+        getmatrix <- function() m
+        lost(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## function casheSolve gets a vector and then uses the solve function
+## to return its inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cashed matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
